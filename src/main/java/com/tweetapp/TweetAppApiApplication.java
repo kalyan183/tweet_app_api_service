b9f6eaada5e1b54f7b35970e5b99eaad6ff2edbb
kalyan183 <ikalyan183@gmail.com>
2022-07-07 15:02:16 +0530

    code refactored

--- src/main/java/com/tweetapp/TweetAppApiApplication.java
+++ src/main/java/com/tweetapp/TweetAppApiApplication.java
@@ -3,6 +3,7 @@ package com.tweetapp;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.TimeZone;
 
--- src/main/java/com/tweetapp/configs/swagger/SwaggerConfig.java
+++ src/main/java/com/tweetapp/configs/swagger/SwaggerConfig.java
@@ -1,17 +1,21 @@
 package com.tweetapp.configs.swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Kalyan Kommulapati
  * @project TweetApp-API
  * @since 06/07/2022 - 07:52 PM
  */
+@EnableSwagger2
+@Component
 public class SwaggerConfig {
 
     @Bean
